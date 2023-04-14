9cb769f8a8a6afd427e7e3829eb80d2985d2bb7d
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-04-14 19:12:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-const containerEl=document.querySelectorAll(".container");
-const careers=["Hello EveryNyan","How are you?","Fine,Thank you","Oh my gah","I wish I were a bird"];
-containerEl.innerHTML= `<h1> i am ${careers[0]}</h1>`;
+body{
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    height: 100vh;
+    align-items: center;
+    flex-direction: column;
+    background-color: rgb(156, 245, 245);
+    
+}
+h1{
+    font-family: 'Sedgwick Ave Display', cursive;
+}
+.anime{
+    display: flex;
+    flex-direction: row;
+     
+}
+.anime1{
+ background-repeat: no-repeat;
+ width: 200px;
+ height: 200px;
+    background: url("https://i1.sndcdn.com/artworks-000081774154-2c17bp-t500x500.jpg");
+    background-size:cover;
+    display: inline-block;
+    padding: 0;
+    transition: 1s;
+    margin: 20px;
+}
+.anime2{
+    background-repeat: no-repeat;
+    width: 200px;
+    height: 200px;
+       background: url("https://pbs.twimg.com/media/FTk0iIPaAAEiLbP.jpg");
+       background-size:cover;
+       padding: 0;
+       transition: 1s;
+       margin: 20px;
+   }
