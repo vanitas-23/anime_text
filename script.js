105950192b60b896ab5689045bd026ca3adda655
Syed <syedkafilurrehman@gmail.com>
2023-04-14 18:36:21 +0530

    first

---
+++ script.js
@@ -0,0 +1,3 @@
+const containerEl=document.querySelectorAll(".container");
+const careers=["Hello EveryNyan","How are you?","Fine,Thank you","Oh my gah","I wish I were a bird"];
+containerEl.innerHTML= `<h1> i am ${careers[0]}</h1>`;
