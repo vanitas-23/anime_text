eb1f9189583e52f2710bc64faeb895996c9168f4
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-04-14 19:13:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
-body{
-    margin: 0;
-    display: flex;
-    justify-content: center;
-    height: 100vh;
-    align-items: center;
-    flex-direction: column;
-    background-color: rgb(156, 245, 245);
+const containerEl=document.querySelector(".container");
+const careers=["Hello EveryNyan","How are you?","Fine! Thank you","Oh my gah","I wish I were a bird"];
+const anime1El=document.querySelector(".anime1");
+const anime2El=document.querySelector(".anime2");
+let careersIndex=0;
+let charIndex=0;
+updateText();
+
+function updateText(){
+    
+    containerEl.innerHTML= `<h1>${careers[careersIndex].slice(0,charIndex)}</h1>`;
+    charIndex++; 
+if(charIndex===careers[careersIndex].length+1){
+anime1El.style.padding="0px";
+}
+    if(charIndex===careers[careersIndex].length+1){
+        careersIndex++;
+        charIndex=0;
+        anime1El.style.padding="0px";
+    }
+    if(careersIndex===careers.length){
+    careersIndex=0;
+    charIndex=0;
+    }
+    anime2El.style.padding="0";
+    anime1El.style.padding="0";
+    if(careersIndex==3){
+        anime2El.style.padding="20px";
+    }
+    else if(careersIndex==0)
+    anime1El.style.padding="0";
+    else{
+        anime1El.style.padding="20px";
+    }
+    setTimeout(updateText,100);
     
 }
-h1{
-    font-family: 'Sedgwick Ave Display', cursive;
-}
-.anime{
-    display: flex;
-    flex-direction: row;
-     
-}
-.anime1{
- background-repeat: no-repeat;
- width: 200px;
- height: 200px;
-    background: url("https://i1.sndcdn.com/artworks-000081774154-2c17bp-t500x500.jpg");
-    background-size:cover;
-    display: inline-block;
-    padding: 0;
-    transition: 1s;
-    margin: 20px;
-}
-.anime2{
-    background-repeat: no-repeat;
-    width: 200px;
-    height: 200px;
-       background: url("https://pbs.twimg.com/media/FTk0iIPaAAEiLbP.jpg");
-       background-size:cover;
-       padding: 0;
-       transition: 1s;
-       margin: 20px;
-   }
